685b7b81a437a9ac04a93a824e5555c383ebc03e
nataliadiaz <diaz.rodriguez.natalia@gmail.com>
2017-10-17 19:31:41 +0200

    infinite loop to run many seqs

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 # this file should be placed in the catkin_ws or ros_ws directory to run it to generate data record sequences of Baxter pushing a button
 for i in {1..150}
-do
+  path_to_save="non_static_button/record_"
   rosrun arm_scenario_experiments button_babbler here
-  mv here/record_0 "here_keep/waif_${i}"
+  mv here/record_0 path_to_save"${i}"
+  echo "new record in "path_to_save"${i}"
 done
