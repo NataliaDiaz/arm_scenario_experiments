5630be0314f218a0e70845701a70cca18c89a30a
nataliadiaz <diaz.rodriguez.natalia@gmail.com>
2017-10-18 17:41:19 +0200

    button changes position now when boolean flag is set in button_babbler file

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # this file should be placed in the catkin_ws or ros_ws directory to run it to generate data record sequences of Baxter pushing a button
-for i in {1..150}
-  path_to_save="non_static_button/record_"
+# THIS FILE SHOULD BE PLACED IN catkin_ws or ros_ws to be run to generate baxter data
+# See execution instructions in arm_scenario_experiments README https://github.com/NataliaDiaz/arm_scenario_experiments
+
+mkdir generated_data
+for i in {1..75}
+do
+  #path_to_save="non_static_button/record_"  # takes name of var? TODO Fix!
   rosrun arm_scenario_experiments button_babbler here
-  mv here/record_0 path_to_save"${i}"
-  echo "new record in "path_to_save"${i}"
+  mv here/record_0 generated_data/record_"${i}"
+  printf "\n\n\n +++++++++++++++++ New record in data/record_${i} !\n "
 done
